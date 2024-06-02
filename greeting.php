34446f115bc03bf16ea7552f0b3c48ad62963a21
raiardia <ardiaanggreni@gmail.com>
2024-06-02 13:03:01 +0800

    update css

--- greeting.php
+++ greeting.php
@@ -1,4 +1,11 @@
-<h1 class="text-center"> Selamat Datang </h1>
-<h3 class="text-center"> Sistem Pakar Penyakit Ayam Metode Forward Chaining </h3>
-<h4 class="text-center"> Ni Made Rai Ardia Anggreni </h4>
-<h5 class="text-center"> 42230019 </h5>
+<div class="greeting-container">
+    <div class="greeting">
+        <div class="text-right">
+            <h4 class="text-center"> Halo! <br>
+            Selamat datang di Sistem Pakar Penyakit Ayam kami! Temukan solusi untuk masalah kesehatan ayam dengan metode Forward Chaining yang praktis. </h4>
+            <h5 class="text-center"> Ni Made Rai Ardia Anggreni </h5>
+            <h6 class="text-center"> 42230019 </h6>
+        </div>
+    </div>
+</div>
+
